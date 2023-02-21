e1f7021049575b36e40b91b9cb85bfbd4e0ea686
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-21 22:13:28 +0100

    First task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - main function entry
+*
+* Return: int(0)
+*/
+Int main(void)
+{
+	char *text = "_putchar";
+	int i;
+
+	for(i = 0; i<text.length;i++)
+		putchar(text[i]);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# This directory is for C tasks day three
+# INTRODUCTION TO Functions & nested loops
