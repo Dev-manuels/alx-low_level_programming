747976a52738da0eaf2543ec14af5713ed665aae
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 03:29:42 +0100

    Test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* jack_bauer - function to print every minute of the day
+*
+* @i: car for loop counter 0 to 2
+* @j: car for loop counter 0 to 3
+* @k: var for loop counter 0 to 5
+* @l: var for loop counter 0 to 9
+* Return: int
+*/
+void jack_bauer(void)
+{
+    int i, j, k, l;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            for (k = 0; k < 6; k++)
+            {
+                for (l = 0; l < 10; l++)
+                {
+                    _putchar('0' + i);
+                    _putchar('0' + j);
+                    _putchar(':');
+                    _putchar('0' + k);
+                    _putchar('0' + l)
+                }
+            }
+        }
+    }
+}
