57a10266ea701ece60f3ce4354dbfff87a775d9f
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 04:08:07 +0100

    Over smart

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,59 +1,45 @@
 #include "main.h"
 /**
 * jack_bauer - function to print every minute of the day
-* jack_bauer_cont - function to continue from the last timer point
+*
+* @i: car for loop counter 0 to 2
+* @j: car for loop counter 0 to 3
+* @k: var for loop counter 0 to 5
+* @l: var for loop counter 0 to 9
 * Return: int
 */
 void jack_bauer(void)
 {
 	int i, j, k, l;
-
 	for (i = 0; i < 3; i++)
 	{
 		if (i == 2)
 		{
-			break;
+			for (j = 0; j < 4; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						printf("%d%d:%d%d\n", i, j, k, l);
+					}
+				}
+			}
 		}
 		else
 		{
+			for (j = 0; j < 9; j++)
 			for (j = 0; j < 10; j++)
 			{
 				for (k = 0; k < 6; k++)
 				{
 					for (l = 0; l < 10; l++)
 					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(':');
-						putchar('0' + k);
-						putchar('0' + l);
-						putchar('\n');
+						printf("%d%d:%d%d\n", i, j, k, l);
 					}
 				}
 			}
 		}
-	}
-	jack_bauer_cont();
-}
-
-void jack_bauer_cont(void)
-{
-	int i, j, k, l;
-	i = 2;
-	
-	for (j = 0; j < 4; j++)
-	{
-		for (k = 0; k < 6; k++)
-		{
-			for (l = 0; l < 10; l++)
-			{
-				putchar('0' + i);
-				putchar('0' + j);
-				putchar(':');
-				putchar('0' + k);
-				putchar('0' + l);
-				putchar('\n');
-			}
-		}
+		
 	}
 }
