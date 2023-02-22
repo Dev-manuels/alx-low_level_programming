83f517e57b8cafac911e352fb7158fad82c6148d
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 02:32:14 +0100

    Return fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		rtval = 1;
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		rtval = 0;
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		rtval = -1;
 	}
 	return (rtval);
