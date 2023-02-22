e3de5ce6ddfbc5bc859eb786853e4a1904db4132
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 02:04:47 +0100

    Attempt task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _isalpha - function to check it char is an alphabet
+*
+* @n: var for comparing
+* Return: int(1)
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	} else
+	{
+		putchar('-');
+		return (0);
+	}
+}
