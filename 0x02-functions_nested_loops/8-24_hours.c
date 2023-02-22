c821819247dc3dc49af36b67b405085cd0d09f1d
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 03:58:17 +0100

    Being smart

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * jack_bauer - function to print every minute of the day
-*
+* jack_bauer_cont - function to continue from the last timer point
 * Return: int
 */
 void jack_bauer(void)
@@ -12,21 +12,8 @@ void jack_bauer(void)
 	{
 		if (i == 2)
 		{
-			for (j = 0; j < 4; j++)
-			{
-				for (k = 0; k < 6; k++)
-				{
-					for (l = 0; l < 10; l++)
-					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(':');
-						putchar('0' + k);
-						putchar('0' + l);
-						putchar('\n');
-					}
-				}
-			}
+			jack_bauer_cont();
+			break;
 		}
 		else
 		{
@@ -44,6 +31,27 @@ void jack_bauer(void)
 						putchar('\n');
 					}
 				}
+			}
+		}
+	}
+}
+
+void jack_bauer_cont(void)
+{
+	int i, j, k, l;
+	i = 2;
+	for (j = 0; j < 4; j++)
+	{
+		for (k = 0; k < 6; k++)
+		{
+			for (l = 0; l < 10; l++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar(':');
+				putchar('0' + k);
+				putchar('0' + l);
+				putchar('\n');
 			}
 		}
 	}
