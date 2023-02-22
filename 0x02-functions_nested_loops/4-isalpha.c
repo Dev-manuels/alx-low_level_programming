e2f37bb153f8a88f5fc9ab4062d31b97a7db32d5
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 01:35:28 +0100

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isalpha - function to check it char s alphabet
+*
+* @c: var for comparing
+ Return: int
+ */
+ int _isalpha(int c)
+ {
+    return (isalpha(c) ? 1 : 0);
+ }
