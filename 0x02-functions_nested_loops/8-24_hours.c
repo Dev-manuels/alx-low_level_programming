50f775db687d9e373a52b1935ccfd2aa22d80a3a
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 03:39:48 +0100

    Testing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,24 +10,46 @@
 */
 void jack_bauer(void)
 {
-    int i, j, k, l;
+	int i, j, k, l;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            for (k = 0; k < 6; k++)
-            {
-                for (l = 0; l < 10; l++)
-                {
-                    putchar('0' + i);
-                    putchar('0' + j);
-                    putchar(':');
-                    putchar('0' + k);
-                    putchar('0' + l);
-                }
-            }
-        }
-        putchar('\n');
-    }
+	for (i = 0; i < 3; i++)
+	{
+		if (i == 2)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(':');
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar('\n');
+					}
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; j < 9; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(':');
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar('\n');
+					}
+				}
+			}
+		}
+		
+	}
 }
