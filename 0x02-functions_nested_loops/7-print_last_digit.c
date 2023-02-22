921fa7e0de47b12d8228a9191b87c18eac36cc71
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 02:51:13 +0100

    Attempt task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* print_last_digit - prints last digit of a number
+* @no:number input to cuntion
+* Return: int
+*/
+int print_last_digit(int no)
+{
+    return (no % 10);
+}
