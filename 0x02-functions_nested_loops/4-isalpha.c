ebf413392286d8e4726964d75aa6664e9b200102
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 01:59:22 +0100

    Task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,8 @@
 * _isalpha - function to check it char is an alphabet
 *
 * @c: var for comparing
- Return: int(1) for yes and 0 for no;
- */
+* Return: int(1) for yes and 0 for no;
+*/
 int _isalpha(int c)
 {
 	if (c > 64 && c < 123)
