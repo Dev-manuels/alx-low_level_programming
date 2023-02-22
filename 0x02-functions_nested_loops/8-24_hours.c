77de67bf2322ff89481f74db0c1c9cc3ae56d585
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-22 03:31:05 +0100

    forgot semi-colon

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,7 +24,7 @@ void jack_bauer(void)
                     _putchar('0' + j);
                     _putchar(':');
                     _putchar('0' + k);
-                    _putchar('0' + l)
+                    _putchar('0' + l);
                 }
             }
         }
