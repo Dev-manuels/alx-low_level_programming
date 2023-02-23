72589d516d856c3fe62c8ded72a8fcd4b058d39f
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 09:02:09 +0100

    testing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,16 +7,23 @@
 void print_times_table(int n)
 {
 	int i, j, out;
+	char str[20];
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			out = i * j;
+			sprintf(str, "%d", i * (j+1));
 			printf("%d", out);
 			if (j == n)
 				continue;
 			putchar(',');
+			if (strlen(str) > 1)
+			{
+				printf("  ");
+				continue;
+			}
 			printf("   ");
 		}
 		putchar('\n');
