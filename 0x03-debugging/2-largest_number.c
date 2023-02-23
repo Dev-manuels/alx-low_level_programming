9f2f0bae454cbd672d5858efa0f1d80f85ea9b31
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 17:36:25 +0100

    Checker is not helping

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
+
 /**
- * main - returns the largest number of the three numbers provided
- * a: first int
- * b: second int
- * c: third int
- * Return: largest number (success)
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a> b && b> c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+    if (a >= b && b >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && a >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-	return (largest);
+    return (largest);
 }
