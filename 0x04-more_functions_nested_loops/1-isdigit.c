22a7d5990141708035c230cc56a1535c7355afa1
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-24 00:46:51 +0100

    Test

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _isdigit -function to test if char c is uppercase
+*
+* @c: varivale to be tested
+* Return: int 0 for NO, 1 for yes
+*/
+int _isdigit(int c)
+{
+
+	return (isdigit(c)) ? 1 : 0;
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* more_numbers - function that prints the numbers, from 0 to 9
+*
+* Return: void
+*/
+void more_numbers(void);
+{
+	
+}
