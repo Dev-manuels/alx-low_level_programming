7b5b0a2353b6a320ed350d94849dda1b787b120e
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 08:00:30 +0100

    First advanced

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_times_table -  function that prints the multiplication table of n
+*
+* Return: void
+*/
+void print_times_table(int n)
+{
+	int i, j, out;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			out = i * j;
+			_putchar('0' + out);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
