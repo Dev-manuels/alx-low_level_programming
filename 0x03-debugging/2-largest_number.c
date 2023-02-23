540e7dfe38725f0648cbbbaf80ef80cea3b9aec4
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 17:06:11 +0100

    Add equality checker

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,10 +14,11 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b >= c)
 		largest = a;
-	else if (b > a && a >= c)
+	else if (b > a && a > c)
 		largest = b;
-	else if (c > a && a >= b)
+	else if (c > a && a > b)
 		largest = c;
-
+	else if (a == b && b == c)
+		largest = a;
 	return (largest);
 }
