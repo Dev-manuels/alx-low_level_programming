3008faf78a4bd0b6b511c186194196b99cd88911
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 20:16:40 +0100

    TEsting

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper - function to test if a letter is uppercase
+*
+* Return: intergr
+*/
+int _isupper(int c)
+{
+    if (isupper(c) >0)
+        return(1);
+    else
+        return(0);
+}
