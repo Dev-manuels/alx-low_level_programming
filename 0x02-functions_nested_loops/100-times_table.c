7f44afb57b997461cb1d2f53e9ff9224858dd6dd
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 12:06:34 +0100

    Betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,6 +2,7 @@
 /**
 * print_times_table -  function that prints the multiplication table of n
 *
+* @n: varible
 * Return: void
 */
 void print_times_table(int n)
@@ -9,7 +10,7 @@ void print_times_table(int n)
 	int i, j, out;
 	char str[20];
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
