8d2c5cc64b9e8a13bfb592430b051ecda0cafe29
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-23 12:23:18 +0100

    test

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - main function entry
+*
+* Return: int SUCCESS
+*/
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("the sum of multiples of 3 & 5 under 1024 is %d",sum);
+	return (0);
+}
