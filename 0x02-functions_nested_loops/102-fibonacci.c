78ee23a332836491dc89e55d04796653afc615d9
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-24 03:38:33 +0100

    test

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,19 @@
 */
 int main(void)
 {
-	int i;
-	long t1 = 1;
-	long t2 = 2;
-	long t3 = t1 + t2;
+int i;
+long t1 = 1;
+long t2 = 2;
+long t3 = t1 + t2;
 
-	printf("%ld, %ld", t1, t2);
-	for (i = 3; i <= 50; i++)
-	{
-		printf(", %ld", t3);
-		t1 = t2;
-		t2 = t3;
-		t3 = t1 + t2;
-	}
-	printf("\n");
-	return (0);
+printf("%ld, %ld", t1, t2);
+for (i = 3; i <= 50; i++)
+{
+printf(", %ld", t3);
+t1 = t2;
+t2 = t3;
+t3 = t1 + t2;
+}
+printf("\n");
+return (0);
 }
