fcbe489365760b15dd6efcdaf3cd366014c581c8
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-24 05:31:24 +0100

    test

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,18 +6,18 @@
 */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0, j;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 				_putchar(' ');
-		putchar('\\');
-		putchar('\n');
+		_putchar(92);
+		_putchar('\n');
 		}
 	}
 	else
-		putchar('\n');
+		_putchar('\n');
 }
