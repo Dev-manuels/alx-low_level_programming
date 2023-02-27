1a8ff7eacc76d4bfc00293cc4a382af230ee91dd
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-27 20:44:08 +0100

    First attempt

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+* reset_to_98 - function that updates the values
+* located at where pointers passed are pointing to
+*
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
