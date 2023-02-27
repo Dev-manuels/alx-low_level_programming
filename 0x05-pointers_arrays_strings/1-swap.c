6482f7f69f4df875a907ca55d895e109c22c8b6e
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-27 20:56:38 +0100

    First attempt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - function that swaps two values a and b
+*
+* @a: varible
+* @b: varible
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = c;
+	*a = b;
+	*b = a;
+}
