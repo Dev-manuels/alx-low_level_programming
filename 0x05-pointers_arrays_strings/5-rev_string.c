655c58e338d98968caf9777b3334497fbf15b804
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-27 21:39:10 +0100

    First attempt

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev - function that reverses a string
+*
+* @s: string to be reversed
+* Return: int
+*/
+void rev_string(char *s)
+{
+	int len = strlen(s), i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
