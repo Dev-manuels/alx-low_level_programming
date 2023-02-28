c89fd7f691dbea93947f94678a9372c3c97b0cc2
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-28 01:14:51 +0100

    Test

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string
- *
- * @str: char to check
- *
- * Return: 0 is success
- */
- void puts2(char *str){
-	int i = 0;
+* puts2 - prints every other character of a string
+*
+* @str: char to check
+*
+* Return: 0 is success
+*/
+void puts2(char *str)
+{
+	int string;
 
-	while ( str[i] != '\0')
-	{
-		printf("%c\n", str[i]);
-	}
- }
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
+}
