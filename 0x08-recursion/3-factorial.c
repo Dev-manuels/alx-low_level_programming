f9e22521a66d22ab8ce0a1ad58ac6577745d5797
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-03-09 15:51:02 +0100

    Fix logic error

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,13 @@
 */
 int factorial(int n)
 {
-	if (n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
 	} else 
 	{
 		return (1 * factorial(n-1));
