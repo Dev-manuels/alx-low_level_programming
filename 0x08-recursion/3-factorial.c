9575ded7d0ecd09ae87a86dba7b201a1e17f2bb0
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-03-09 15:57:42 +0100

    Betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,12 +10,11 @@ int factorial(int n)
 	if (n < 0)
 	{
 		return (-1);
-	}
-	else if (n == 0)
+	} else if (n == 0)
 	{
 		return (1);
 	} else
 	{
-		return (1 * factorial(n-1));
+		return (1 * factorial(n - 1));
 	}
 }
