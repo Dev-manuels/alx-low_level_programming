2d0cf4127f1c185026360c55ded3700ef8305d15
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-03-13 17:17:08 +0100

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strdup - function that returns a pointer to a newly
+ * allocated space in memory, which contains a copy of
+ * the string given as a parameter.
+ * @str: string to be copied
+ * Return: copy of the string passed.
+*/
+char *_strdup(char *str)
+{
+	int size = strlen(str);
+	char *s = malloc(size);
+	int i;
+
+	if (s != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			s[i] = str[i];
+		}
+	}
+	return (s);
+}
