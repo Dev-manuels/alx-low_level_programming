5d28d08d31b654d00f57b2c9b3b33e8035bb6e33
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-03-14 13:37:41 +0100

    Fixed compilation bugs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,16 +19,21 @@ int **alloc_grid(int width, int height)
 		{
 			for (i = 0; i < height; i++) {
 				if (arr[i] != NULL)
+				{
 					arr[i] = malloc(width * sizeof(int));
 					if (arr[i] == NULL)
 						return (NULL);
-				else
+				} else
+				{
 					return (NULL);
+				}
 			}
 			for (i = 0; i < height; i++)
 			{
 				for (j = 0; j < width; j++)
+				{
 					arr[i][j] = 0;
+				}
 			}
 		} else
 		{
