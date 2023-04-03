14c821ecb3a1e1d27593693bdd7f9f5e67a550e6
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-04-03 20:32:56 +0100

    Correct function name

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,15 +7,18 @@
  * @head: pointer to start of list
  * Return: pointer to new node
 */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (head != NULL && new != NULL)
+	if (head != NULL)
 	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
+		if (new != NULL)
+		{
+			new->n = n;
+			new->next = *head;
+			*head = new;
+		}
 	}
 	return (new);
 }
