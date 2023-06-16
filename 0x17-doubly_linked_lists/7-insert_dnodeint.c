6e753b5a61f2827c579e5d8940e0326c537bfe60
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-06-16 06:02:24 +0100

    improve logic

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	} else
 	{
 		new_node->next = current;
+		*h = new_node;
 		return (new_node);
 	}
 	free(new_node);
