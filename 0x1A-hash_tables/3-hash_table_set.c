3ba16966468c6e4246847db7eb3ddeff3f6c74e8
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-07-09 02:06:14 +0100

    handle when key already exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @head: pointer to key hash index of the hash table
  * @key: key to value
  * @value: values linked to key
- * Return: pointer to new node
+ * Return: 1 on success else 0
 */
 int add_node(hash_node_t **head, const char *key, const char *value)
 {
@@ -33,13 +33,43 @@ int add_node(hash_node_t **head, const char *key, const char *value)
 
 	if (head != NULL)
 	{
-		if (new != NULL)
+		if (new != NULL && update_node(head, key, value) == 0)
 		{
 			new->key = (char *) key;
 			new->value = (char *) value;
 			new->next = *head;
 			*head = new;
 			status = 1;
+		}
+	}
+	return (status);
+}
+
+/**
+ * update_node - function that updates the value
+ * of a node in a hash table
+ * @head: pointer to key hash index of the hash table
+ * @key: key to the value to be updated
+ * @value: new value
+ * Return: 1 on success else 0
+*/
+int update_node(hash_node_t **head, const char *key, const char *value)
+{
+	int status = 0;
+	hash_node_t *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while (current)
+		{
+			if (strcmp(current->key, key) == 0)
+			{
+				current->value = (char *) value;
+				status = 1;
+				break;
+			}
+			current = current->next;
 		}
 	}
 	return (status);
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -39,5 +40,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int add_node(hash_node_t **head, const char *key, const char *value);
+int update_node(hash_node_t **head, const char *key, const char *value);
 
 #endif
