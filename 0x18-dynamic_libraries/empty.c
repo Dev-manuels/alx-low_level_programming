916b6f8eb31d518326ba1a3d8e93e337e3262de5
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-22 11:04:44 +0100

    first commit

---
+++ 0x18-dynamic_libraries/empty.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * _putchar - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _putchar(char c)
+{
+    if (c)
+        return (1);
+    else
+        return (0);
+}
+/**
+ * _islower - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _islower(int c)
+{
+    if (c)
+        return (1);
+    else
+        return (0);
+}
+/**
+ * _isalpha - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _isalpha(int c)
+{
+    if (c)
+        return (1);
+    else
+        return (0);
+}
+/**
+ * _abs - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _abs(int n)
+{
+    if (n > 0)
+        return (1);
+    else
+        return (0);
+}
+/**
+ * _isupper - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _isupper(int c)
+{
+    if (c)
+        return (1);
+    else
+        return (0);
+}
---
+++ 0x18-dynamic_libraries/empty2.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _isdigit - empty function
+ * @c: dummy arg
+ * Return: int
+*/
+int _isdigit(int c)
+{
+    return (0);
+}
+/**
+ * _strlen - empty function
+ * @s: dummy arg
+ * Return: int
+*/
+int _strlen(char *s)
+{
+    return (0);
+}
+/**
+ * _puts - empty function
+ * @s: dummy arg
+*/
+void _puts(char *s)
+{
+    
+}
+/**
+ * _strcpy - empty function
+ * @dest: dummy arg
+ * @src: dummy arg
+ * Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+    return (dest);
+}
+/**
+ * _atoi - empty function
+ * @s: dummy arg
+ * Return: int
+*/
+int _atoi(char *s)
+{
+    return (0);
+}
