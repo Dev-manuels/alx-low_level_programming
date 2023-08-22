6d5e2d852f7dfcd5fb07a56edc30307a1cbc66ec
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-22 17:55:32 +0100

    update script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/libsh.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmal.so
-export LD_PRELOAD=/tmp/libsh.so
+wget -qO /tmp/liball.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD=/tmp/liball.so
