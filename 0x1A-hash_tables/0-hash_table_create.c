b3cf49d279bad4dbe081b4726d44becf8836cd56
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-07 10:39:14 +0100

    add hash_tables project

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -function that creates a hash table
+ * @size: size of the array
+ * Return: Null on failure else new has table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+
+	if (size < 1 || ht == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		free(ht);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		(ht->array)[i] = NULL;
+		i++;
+	}
+	return (ht);
+}
